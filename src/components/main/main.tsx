41192f65b819f6b290da70c8cb186255df8abadd
DigamaSyx <auga1sports@gmail.com>
2023-03-12 14:14:19 -0300

    Added a simple logic when its in initial page don't render the header

--- src/components/main/main.tsx
+++ src/components/main/main.tsx
@@ -2,15 +2,16 @@
  * @format
  */
 
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, useLocation } from 'react-router-dom';
 import { Calculator } from '../paths/pathWrapper';
 import { Home } from '../wrapper';
 import { Header } from './components/header/header';
 
 export default function Main() {
+	const a = useLocation().pathname;
 	return (
 		<main className="main_content">
-			<Header />
+			{a === '/' ? '' : <Header />}
 			<Routes>
 				<Route path="/" element={<Home />} />
 				<Route path="/calculator" element={<Calculator />} />
